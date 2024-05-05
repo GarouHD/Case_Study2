b50a2d0b3e952c2752fbacbd71f35fce64bbee52
GarouHD <107570218+GarouHD@users.noreply.github.com>
2024-05-04 23:59:22 -0400

    Agglomerative clustering analysis complete

--- template_predictVessel.py
+++ template_predictVessel.py
@@ -4,6 +4,13 @@ from sklearn import preprocessing
 from sklearn.cluster import KMeans
 import functools
 from sklearn.metrics.cluster import adjusted_rand_score
+from sklearn import preprocessing
+from sklearn.cluster import AgglomerativeClustering
+import functools
+from sklearn.metrics.cluster import adjusted_rand_score
+from sklearn.preprocessing import StandardScaler
+from itertools import combinations
+import matplotlib.pyplot as plt
 
 
 def hh_mm_ss2seconds(hh_mm_ss):
@@ -14,7 +21,7 @@ def predictor_baseline(csv_path):
     # load data and convert hh:mm:ss to seconds
     df = pd.read_csv(csv_path, converters={'SEQUENCE_DTTM' : hh_mm_ss2seconds})
     # select features 
-    selected_features = ['SEQUENCE_DTTM', 'LAT', 'LON', 'SPEED_OVER_GROUND' ,'COURSE_OVER_GROUND']
+    selected_features = ['SEQUENCE_DTTM', 'LAT', 'LON', 'SPEED_OVER_GROUND','COURSE_OVER_GROUND']
     X = df[selected_features].to_numpy()
     # Standardization 
     X = preprocessing.StandardScaler().fit(X).transform(X)
@@ -36,6 +43,16 @@ def get_baseline_score():
         print(f'Adjusted Rand Index Baseline Score of {file_name}: {rand_index_score:.4f}')
 
 
+def get_predictor_score():
+    file_names = ['set1.csv', 'set2.csv']
+    for file_name in file_names:
+        csv_path = './Data/' + file_name
+        labels_true = pd.read_csv(csv_path)['VID'].to_numpy()
+        labels_pred = predictor(csv_path)
+        rand_index_score = adjusted_rand_score(labels_true, labels_pred)
+        print(f'Adjusted Rand Index Predictor Score of {file_name}: {rand_index_score:.4f}')
+
+
 def evaluate():
     csv_path = './Data/set3.csv'
     labels_true = pd.read_csv(csv_path)['VID'].to_numpy()
@@ -45,12 +62,25 @@ def evaluate():
 
 
 def predictor(csv_path):
-    # fill your code here
+
+    # Pre-Process Data
+    df = pd.read_csv(csv_path, converters={'SEQUENCE_DTTM': hh_mm_ss2seconds})
+    selected_features = ['LAT', 'LON', 'SPEED_OVER_GROUND', 'COURSE_OVER_GROUND']
+    X = df[selected_features]
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    # Create agglomerative clustering model with optimal param
+    model = AgglomerativeClustering(n_clusters=20, linkage='complete', metric='cosine')
+
+    # predict
+    labels_pred = model.fit_predict(X)
     return labels_pred
 
 
 if __name__=="__main__":
     get_baseline_score()
+    get_predictor_score()
     evaluate()
 
 
